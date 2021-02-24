fe76a07f5eb8bf7582b578291123e65ce31ad2c0
rajesh196rsh <56480220+rajesh196rsh@users.noreply.github.com>
2021-02-24 14:57:55 +0530

    Update settings.py

--- cjapp/settings.py
+++ cjapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^7a+-jxtie_^$wb-pb^hgpr@o%(q55e089q#9g)jy@(y-x590g'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
